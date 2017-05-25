02cd4a4affea5f82ef4e8949bdde06f690aa8704
Jesse Michael <jmichael@brightcove.com>
2017-05-25 16:50:42 -0700

    magic cars 🚗

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	public GameObject plane;
+	public GameObject transportType;
+
+	public Transport current;
+	public Vector3 lastPoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Mouse Drag Event
+		if (Input.GetMouseButton(0))
+		{
+			RaycastHit hit;
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+			{
+				Debug.Log("Mouse down. Raycast: " +  hit.point);
+				if (current == null) {
+					lastPoint = hit.point;
+					current = newTransport(hit.point);
+				} else {
+					
+				}
+			}
+		}
+
+		// Mouse Up Event
+		if (Input.GetMouseButtonUp(0)) {
+			current = null;
+		}
+	}
+
+	private Transport newTransport(Vector3 point) {
+		Debug.Log("New Transport");
+		lastPoint = point;
+		GameObject newTransport =  Instantiate (transportType, Vector3.one, Quaternion.Euler (Vector3.zero));
+		return newTransport.GetComponent<Transport>();
+	}
+}
---
+++ Assets/Scripts/Transport.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transport : MonoBehaviour {
+
+	public Queue<Vector3> current;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Run the Transport by making it follow the buffer of points
+	void Run() {
+
+	}
+}
